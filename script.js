adf70f653551d67898379986a0a7ff407c7fbab2
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-21 14:01:04 +0200

    Task completed

---
+++ script.js
@@ -0,0 +1,75 @@
+console.log('funguju!');
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+
+console.log('Zacinam')
+
+const btnClick = () => {
+  fetch('https://api.sunrise-sunset.org/json?lat=50&lng=14.5')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Vypisuji vychod')
+
+      sunrise.textContent = `Vychod slunce: ${data.results.sunrise}`
+      sunset.textContent = `Zapad slunce: ${data.results.sunset}`
+    })
+}
+
+
+
+document.querySelector('.showBtn').addEventListener('click', btnClick);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const odpoved = document.querySelector('.odpoved')
+
+const zpracujJSON = (data) => {
+  console.log('Tady jsou data ze serveru')
+  console.log(data.results.sunrise)
+  odpoved.textContent = data.results.sunrise
+}
+
+const zpracujOdpoved = (odpoved) => {
+  odpoved.textContent = 'Hura, mame data'
+  // console.log(odpoved)
+  const jsonPrislib = odpoved.json()
+  jsonPrislib.then(zpracujJSON)
+}
+
+const promise = fetch('https://api.sunrise-sunset.org/json?lat=50&lng=14.5')
+odpoved.textContent = 'Strpeni, nacitam'
+promise.then(zpracujOdpoved)
+*/
+
+/*
+//ZKRACENY ZAPIS
+
+fetch('https://api.sunrise-sunset.org/json?lat=50&lng=14.5')
+  .then((odpoved) => odpoved.json())
+  .then((data) => {
+    console.log(data)
+    odpoved.textContent = data.results.sunrise
+  })
+
+//PRIKLAD 2.
+
+const dnyDoVanoc = document.querySelector('.dnyDoVanoc')
+
+fetch('https://just-countdown.eu/api/control?id=vanoce')
+  .then((odpoved) => odpoved.json())
+  .then((data) => {
+    const hodiny = dnyDoVanoc.textContent = data.roomState.remainingTime.hours
+    dnyDoVanoc.textContent = Math.round(hodiny / 24)
+  })
+*/
